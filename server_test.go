7e9f36ce5f1524b86cb423104bd12056eb6cf4b8
Jonah Saltzman <jonahsaltzman@gmail.com>
2023-04-07 19:18:52 -0400

    benchmarks

--- server_test.go
+++ server_test.go
@@ -1,13 +1,53 @@
 package main
 
-// import (
-// 	"testing"
-// 	"net/http/httptest"
-// 	"net/http"
-// )
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
 
-// func TestJoinRoomHandler(t *testing.T) {
-// 	req := httptest.NewRequest(http.MethodGet, "/join", nil)
+var s *Server
+
+var goodLogin, _ = json.Marshal(LoginRequest{
+	User:     "jonah",
+	Password: "look24",
+})
+
+func TestMain(m *testing.M) {
+	server, err := createServer("look24")
+	if err != nil {
+		os.Exit(1)
+	}
+	s = server
+	code := m.Run()
+	os.Exit(code)
+}
+
+// func TestLoginHandler(t *testing.T) {
+// 	req := httptest.NewRequest(http.MethodPost, "/login", bytes.NewReader(goodLogin))
+// 	req.Header.Set("Content-Type", "application/json")
 // 	w := httptest.NewRecorder()
-
+// 	handler := http.HandlerFunc(s.loginHandler)
+// 	handler.ServeHTTP(w, req)
+// 	if w.Code != 200 {
+// 		t.Errorf("non-200 status code")
+// 	}
+// 	token := w.Body.String()
+// 	if len(token) <= 0 {
+// 		t.Errorf("token '%s' invalid", token)
+// 	}
 // }
+
+func BenchmarkLogin(b *testing.B) {
+	req := httptest.NewRequest(http.MethodPost, "/login", bytes.NewReader(goodLogin))
+	req.Header.Set("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+	handler := http.HandlerFunc(s.loginHandler)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		handler(w, req)
+	}
+}
